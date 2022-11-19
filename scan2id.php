99a3cc12e1764209b8fc323adad72d08426acc79
jeroen weij <jeroenweij88@gmail.com>
2022-11-19 20:18:03 +0100

    add barcode scanner test

--- scan2id.php
+++ scan2id.php
@@ -2,8 +2,16 @@
 
 function scan2id($input)
 {
-    if (strlen($input) > 80 ){
+    if (str_starts_with($input, "[)>") && str_contains($input, "?") ){
         // Data matrix
+        $splitted = explode("?", $input);
+        foreach ($splitted as $value) {
+            if (str_starts_with($value, "1P"))
+            {
+                $input = $value;
+                break;
+            }
+        }
     }
 
     if (str_starts_with($input, "1P"))
